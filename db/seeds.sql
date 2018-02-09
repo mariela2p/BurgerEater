3833b9f62786d6fa7c6fe56d8c15768898506dff
Mariela Pascual <marielapascual@Marielas-MBP.the-beach.net>
2018-02-09 18:32:05 -0500

    added db and some basics

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+            VALUES("Veggie Portobello Mushroom Burger", true, "02-11-2018");
+
+INSERT INTO burgers(burger_name, devoured, date)
+            VALUES("Vegan Black Bean Burger", true, "02-12-2018");
+
+INSERT INTO burgers(burger_name, devoured, date)
+            VALUES("Vegetable Veggie Burger", false, "02-13-2018");
